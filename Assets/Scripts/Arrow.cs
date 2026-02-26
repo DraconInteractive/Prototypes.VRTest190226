21026cb40847ce69a50517452491afc60523f52d
Peter Carey <petermarkcarey@gmail.com>
2026-02-26 17:13:08 +0800

    feat: remove rb from arrow, simulate physics and raycasts and add aim marker

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -4,10 +4,12 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Arrow : MonoBehaviour
 {
-    public float fireForce = 10;
+    public float fireSpeed = 10;
     public float lifeTime = 3;
     public float raycastDist = 0.5f;
     public float digLength = 0.05f;
+
+    public GameObject aimMarker;
     
     public LayerMask physicsLayer;
     
@@ -17,13 +19,15 @@ public class Arrow : MonoBehaviour
     private bool _released;
     private bool _hit;
     
-    private Rigidbody rb;
     public void Init(XRPullInteractable pullInteractable, XRGrabInteractable bowInteractable)
     {
         _pullInteractable = pullInteractable;
         _bowInteractable = bowInteractable;
         _pullInteractable.PullUpdated += OnPullUpdated;
         _pullInteractable.PullActionReleased += OnRelease;
+        
+        aimMarker.transform.SetParent(null);
+        aimMarker?.SetActive(false);
     }
 
     private void OnDestroy()
@@ -33,22 +37,53 @@ public class Arrow : MonoBehaviour
             _pullInteractable.PullUpdated -= OnPullUpdated;
             _pullInteractable.PullActionReleased -= OnRelease;
         }
+
+        if (aimMarker != null)
+        {
+            Destroy(aimMarker);
+        }
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (!_released)
         {
+            // If valid target found in trajectory, show marker. Else hide.
+            var markerRay = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(markerRay, out var hit, (fireSpeed * lifeTime * 1.15f), physicsLayer.value))
+            {
+                if (!aimMarker.activeSelf)
+                {
+                    aimMarker.SetActive(true);
+                }
+                aimMarker.transform.position = hit.point;
+            }
+            else if (aimMarker.activeSelf)
+            {
+                aimMarker.SetActive(false);
+            }
             return;
         }
 
+        
         if (!_hit)
         {
-            var ray = new Ray(transform.position, transform.forward);
-            if (Physics.Raycast(ray, out var hit, raycastDist, LayerMask.GetMask()))
+            // TODO: Separate physics and render / 'true' position
+            // So in fixed update update the 'physics' position. This is used for collisions etc
+            // In normal update, move the arrows actual body towards the physics position smoothly
+            // If we want, we can even move the arrow slightly forward of the current physics position (if we wanted predictive rendering). Not required for now
+            
+            Vector3 startPos = transform.position;
+            float stepDist = fireSpeed * Time.fixedDeltaTime;
+
+            transform.position += transform.forward * stepDist;
+            
+            // Using cached pos so that it checks along the move trajectory already covered, not just distance in front of the arrow
+            // If the item hit is in front of the arrow it will jump forward, forcing the contact instead of receiving it
+            var ray = new Ray(startPos, transform.forward);
+            if (Physics.Raycast(ray, out var hit, stepDist + raycastDist, physicsLayer.value))
             {
-                transform.position += -(hit.normal * digLength);
-                Destroy(rb);
+                transform.position += transform.forward * digLength;
                 _hit = true;
             }
         }
@@ -87,8 +122,7 @@ public class Arrow : MonoBehaviour
         _pullInteractable.PullActionReleased -= OnRelease;
 
         transform.SetParent(null);
-        rb = gameObject.AddComponent<Rigidbody>();
-        rb.AddForce(transform.forward * fireForce, ForceMode.Impulse);
         Destroy(gameObject, lifeTime);
+        Destroy(aimMarker);
     }
 }
